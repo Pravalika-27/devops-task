2688fad16de9c4ade9f6fcfc25b85e10cdd095c3
Pravalika <112800726+Pravalika-27@users.noreply.github.com>
2025-09-13 17:31:06 +0530

    Delete infrastructure directory

--- infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.service.name
-}
-
-output "task_definition_family" {
-  value = aws_ecs_task_definition.task.family
-}
--- infrastructure/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID where ECS will be deployed"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet ID (must be public) to deploy ECS tasks"
-  type        = string
-}
